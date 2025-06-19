2dc01d9b977ba00d5534d56040c5af4dbbcf1bff
Your Name <you@example.com>
2025-06-19 22:09:25 +0300

    first commit

---
+++ app.ts
@@ -0,0 +1,87 @@
+import express, {Express} from "express";
+import path from "path";
+import morgan from "morgan"
+import fs from "fs"
+
+// Create type
+type TUsers = {
+    name : string;
+    email : string;
+};
+// Create users empty list
+let users: TUsers [] = []
+
+fs.readFile("data/users.json", "utf8", (err: NodeJS.ErrnoException | null, data: string) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    try {
+        users = JSON.parse(data)
+        console.log("Has been read: "+ JSON.stringify(users));
+    } catch (error: any) {
+        console.error(`Error parsing JSON: ${error}`)
+    }
+})
+
+
+const app: Express = express();
+const port = 3000;
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"))
+
+
+app.get('/hello', function(req, res) {
+	res.send({
+        msg: "Hello world!"
+    });
+});
+
+
+app.get('/echo/:id', function(req, res) {
+	res.send({
+        id: "dog"
+    });
+});
+
+
+app.post('/sum/', function (req, res) { 
+    let mass=req.body.numbers;
+    mass=mass.slice(1, mass.length-1);
+    let arr=mass.split(',').map(Number);
+    let sum=0;
+    for (let i=0; i<arr.length; i++){
+        sum+=Number(arr[i]);
+    }
+    
+    console.log(sum);
+    res.send({"sum":sum});
+});
+
+app.post('/users/', function requestHandler(req, res) {
+
+    res.send(req.body);
+    users.push(req.body)
+    console.log(users);
+
+    fs.writeFile("data/users.json", JSON.stringify(users), (err: NodeJS.ErrnoException | null) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        res.json(users)
+    }) 
+});
+
+app.get('/users', function(req, res) {
+	//res.send(users);
+    //res.status(201).json(users);
+    res.status(201).send(users);
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
---
+++ public/js/main.js
@@ -0,0 +1,45 @@
+
+const submitButton = document.getElementById("submitButton")
+submitButton.addEventListener("click", async () => {
+
+    const userName = document.getElementById("userName");
+    const userEmail = document.getElementById("userEmail");
+    const person = {
+        "name": userName.value,
+        "email": userEmail.value
+    };
+
+    const userData = await fetch("http://localhost:3000/users", {
+        method: "post",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(person)
+    })
+    const responceFromUser = await userData.json()
+    console.log(responceFromUser)
+    userName.value = ""
+    userEmail.value = ""
+    document.getElementById("submitButton").disabled = false
+})
+
+const addPoemButtonFromAPI = document.getElementById("getUserButton")
+addPoemButtonFromAPI.addEventListener("click", async function () {
+
+    const usersData = await fetch("http://localhost:3000/users")
+    const usersJson = await usersData.json()
+    console.log(usersData);
+
+    var ul = document.getElementById("userList");
+
+    while (ul.hasChildNodes()) {
+        ul.removeChild(ul.firstChild);
+    }
+    
+    var ul = document.getElementById("userList");
+    usersJson.forEach(user_ => {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(JSON.stringify(user_.name+" - "+user_.email)));
+        ul.appendChild(li);
+    });
+})
